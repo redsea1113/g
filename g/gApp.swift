6374f62498fdbf6497203400214a5979609234a9
Ali Farhat <tfdc2003@gmail.com>
2023-06-24 08:49:59 +0400

    Initial Commit

---
+++ g/gApp.swift
@@ -0,0 +1,17 @@
+//
+//  gApp.swift
+//  g
+//
+//  Created by Ali Farhat on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct gApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
